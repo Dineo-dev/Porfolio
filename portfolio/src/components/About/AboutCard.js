a0d7775a840e8af6ebfa46f924af84a9670db021
Dineo <dineo.phahla@dadevs.co.za>
2023-05-25 09:26:50 +0200

    About done

---
+++ portfolio/src/components/About/AboutCard.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hello Everyone, I am <span className="purple">Dineo Phahla </span>
+            from <span className="purple"> Johannesburg, South Africa.</span>
+            <br />I am a junior Full Stack Software Developer.
+            <br />
+            <br />
+            Apart from writing lines of coding, these are some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Reading
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Dineo</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
